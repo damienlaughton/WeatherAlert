b50eca5d47752112e76a54378680e67cdbe44466
Damien Laughton <dfplaughton@googlemail.com>
2016-02-29 14:22:26 +0000

    Initial Commit

---
+++ weatheralert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatheralert
+//
+//  Created by Damien Laughton on 29/02/2016.
+//  Copyright © 2016 Damien Laughton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
