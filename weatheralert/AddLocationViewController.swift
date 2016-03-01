98df80c0dabd1748c17a9e0f67e588e8e5cd8734
Damien Laughton <dfplaughton@googlemail.com>
2016-03-01 13:26:10 +0000

    added fields and button to Add Location VC

--- weatheralert/AddLocationViewController.swift
+++ weatheralert/AddLocationViewController.swift
@@ -10,4 +10,35 @@ import UIKit
 
 class AddLocationViewController: RootViewController {
 
+  @IBOutlet weak var nameUITextField: UITextField!
+  @IBOutlet weak var countryUITextField: UITextField!
+  @IBOutlet weak var addLocationButton: UIButton!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    enableDisableAddLabelButton(self)
+  }
+  
+  @IBAction func enableDisableAddLabelButton (sender: AnyObject) {
+    var nameFieldHasText: Bool = false
+    if let locationName = nameUITextField.text {
+      if locationName.characters.count > 0 {
+        nameFieldHasText = true
+      }
+    }
+    
+    addLocationButton.enabled = nameFieldHasText
+    
+  }
+  
+//  MARK: - IBAction(s)
+  
+  @IBAction func addLocation(sender: UIButton) {
+  
+  }
+  
+  
 }
